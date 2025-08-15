a8e74c11981484b69f68c62258040e2e52665649
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-15 16:36:33 -0300

    feat: início da programação no visual code

---
+++ tipagem.py
@@ -0,0 +1,18 @@
+# tipagem dinâmica.
+
+# LIMPAR O TERMINAL
+import os
+os.system("cls")
+
+nome = input("digite seu nome: ")
+idade = input("digite sua idade: ")
+altura = input("digite sua altura: ")
+país = input("digite seu país: ")
+
+
+
+
+print(f"nome: {nome}")
+print(f"idade: {idade}")
+print(f"altura: {altura}")
+print(f"país: {país}")
